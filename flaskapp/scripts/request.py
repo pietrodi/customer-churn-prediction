ef6a6844af35b27b2a542377f1cfb50334eb98b8
pietrodi <dan.pietro@outlook.it>
2022-09-30 18:29:17 +0200

    Add script for sample prediction request testing

---
+++ flaskapp/scripts/request.py
@@ -0,0 +1,34 @@
+import requests
+
+LOCAL_URL = 'http://localhost:9696/predict'
+SAMPLE_CUSTOMER = {
+    'CustomerId': 83131903810,
+    'Surname': 'White',
+    'CreditScore': 630,
+    'Geography': 'Spain',
+    'Gender': 'Female',
+    'Age': 55,
+    'Tenure': 2,
+    'Balance': 223111.10,
+    'NumOfProducts': 1,
+    'HasCrCard': 1,
+    'IsActiveMember': 0,
+    'EstimatedSalary': 80100.77
+}
+
+def send_request(json, url):
+    """
+    Sends a json in a post request to the
+    specified url and returns the json response.
+
+    Args:
+        json: The json request
+        url: The url to send the request to
+
+    Returns:
+        The json response to the request
+    """
+    return requests.post(url, json = json).json()
+
+if __name__ == "__main__":
+    print(send_request(SAMPLE_CUSTOMER, LOCAL_URL))
